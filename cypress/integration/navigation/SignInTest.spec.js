54d15fd7ea0f63544688a148ab52be788e3305ef
Pavan Kumar <npavankumar@deloitte.com>
2019-11-28 16:25:53 +0530

    New test added with invalid credentials

--- cypress/integration/navigation/SignInTest.spec.js
+++ cypress/integration/navigation/SignInTest.spec.js
@@ -14,6 +14,22 @@ describe('Sign In', () => {
             .contains('Invalid login, please try again');
     });
 
+    it("should not sign in with incorrect credentials" , () =>{
+        const home = new HomePage();
+        home.visit();
+
+        const signIn = home.goToSignIn();
+
+        signIn.fillEmail("username")
+        .fillPassword("waste2020")
+        .submit();
+
+        signIn.getLoginError()
+            .should('exist')
+            .contains('Invalid login, please try again');
+    });
+
+
     it("should sign in with correct credentails", () => {
         const home = new HomePage();
         home.visit();
